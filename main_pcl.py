2c1f5afc4e3c4573a3de98e7b7eb726d8eb4b46d
juz031 <junruz@andrew.cmu.edu>
2024-01-26 19:05:34 -0500

    add options for faiss clustering

--- main_pcl.py
+++ main_pcl.py
@@ -91,6 +91,8 @@ parser.add_argument('--exp-dir', default='/user_data/junruz/experiment_pcl', typ
                     help='experiment directory')
 parser.add_argument('--shape', action='store_true',
                     help='Use shape to cluster')
+parser.add_argument('--faissgpu', action='store_true',
+                    help='Use faiss gpu')
 
 
 def main():
@@ -245,7 +247,10 @@ def main():
 
             features[torch.norm(features, dim=1) > 1.5] /= 2  # account for the few samples that are computed twice
             features = features.numpy()
-            cluster_result = run_kmeans(features, args)  # run kmeans clustering on master node
+            if args.faissgpu:
+                cluster_result = run_kmeans_gpu(features, args)
+            else:
+                cluster_result = run_kmeans_cpu(features, args)  # run kmeans clustering on master node
             # save the clustering result
             if (epoch + 1) % 10 == 0:
                 torch.save(cluster_result, os.path.join(args.exp_dir, 'clusters_%d' % epoch))
@@ -346,7 +351,81 @@ def compute_features(eval_loader, model, args):
     return features.cpu()
 
 
-def run_kmeans(x, args):
+def run_kmeans_cpu(x, args):
+    """
+    Args:
+        x: data to be clustered
+    """
+
+    print('performing kmeans clustering')
+    results = {'im2cluster': [], 'centroids': [], 'density': []}
+
+    for seed, num_cluster in enumerate(args.num_cluster):
+        # intialize faiss clustering parameters
+        d = x.shape[1]
+        k = int(num_cluster)
+        clus = faiss.Clustering(d, k)
+        clus.verbose = True
+        clus.niter = 20
+        clus.nredo = 5
+        clus.seed = seed
+        clus.max_points_per_centroid = 1000
+        clus.min_points_per_centroid = 4
+        index = faiss.IndexFlatL2(d)
+
+        # res = faiss.StandardGpuResources()
+        # cfg = faiss.GpuIndexFlatConfig()
+        # cfg.useFloat16 = False
+        # cfg.device = 0
+        # index = faiss.GpuIndexFlatL2(res, d, cfg)
+        
+
+        # clus.train(x, index)
+        clus.train(x, index)
+
+        D, I = index.search(x, 1)  # for each sample, find cluster distance and assignments
+        im2cluster = [int(n[0]) for n in I]
+
+        # get cluster centroids
+        centroids = faiss.vector_to_array(clus.centroids).reshape(k, d)
+
+        # sample-to-centroid distances for each cluster
+        Dcluster = [[] for c in range(k)]
+        for im, i in enumerate(im2cluster):
+            Dcluster[i].append(D[im][0])
+
+        # concentration estimation (phi)
+        density = np.zeros(k)
+        for i, dist in enumerate(Dcluster):
+            if len(dist) > 1:
+                d = (np.asarray(dist) ** 0.5).mean() / np.log(len(dist) + 10)
+                density[i] = d
+
+                # if cluster only has one point, use the max to estimate its concentration
+        dmax = density.max()
+        for i, dist in enumerate(Dcluster):
+            if len(dist) <= 1:
+                density[i] = dmax
+
+        density = density.clip(np.percentile(density, 10),
+                               np.percentile(density, 90))  # clamp extreme values for stability
+        density = args.temperature * density / density.mean()  # scale the mean to temperature
+
+        # convert to cuda Tensors for broadcast
+        centroids = torch.Tensor(centroids).cuda()
+        centroids = nn.functional.normalize(centroids, p=2, dim=1)
+
+        im2cluster = torch.LongTensor(im2cluster).cuda()
+        density = torch.Tensor(density).cuda()
+
+        results['centroids'].append(centroids)
+        results['density'].append(density)
+        results['im2cluster'].append(im2cluster)
+
+    return results
+
+
+def run_kmeans_gpu(x, args):
     """
     Args:
         x: data to be clustered
@@ -367,12 +446,12 @@ def run_kmeans(x, args):
         clus.max_points_per_centroid = 1000
         clus.min_points_per_centroid = 4
 
-        # res = faiss.StandardGpuResources()
-        # cfg = faiss.GpuIndexFlatConfig()
-        # cfg.useFloat16 = False
-        # cfg.device = 0
-        # index = faiss.GpuIndexFlatL2(res, d, cfg)
-        index = faiss.IndexFlatL2(d)
+        res = faiss.StandardGpuResources()
+        cfg = faiss.GpuIndexFlatConfig()
+        cfg.useFloat16 = False
+        cfg.device = 0
+        index = faiss.GpuIndexFlatL2(res, d, cfg)
+        
 
         # clus.train(x, index)
         clus.train(x, index)
